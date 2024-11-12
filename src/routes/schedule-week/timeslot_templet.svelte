4ced5e0b655b1e702c5af6d573706f45f41c8965
tobia <tlva23@student.aau.dk>
2024-11-12 15:03:04 +0100

    Date

---
+++ src/routes/schedule-week/timeslot_templet.svelte
@@ -0,0 +1,42 @@
+<script>
+
+    //import from Database 
+    //import {timeslot}
+    
+    let scheduleItems = [
+    {
+      title: "Default 2024",
+      startTime: "6:00",
+      endTime: "19:00",
+      color: "schedule-color-blue",
+      size: "schedule-size-3",
+    },
+    
+  ];
+
+
+    let timeSlots = &state([]);
+    timeslot.subscribe((value)) => {
+        timeSlots = value;
+    }
+
+    
+
+
+
+</script>
+
+
+
+<div class="schedule-container">
+    {#each scheduleItems as item}
+     <div class={`schedule-week-schema-row-item ${item.color} ${item.size}`}>
+        <div class="schedule-week-schema-row-item-title">
+          {item.title}
+      </div>
+     <div class="schedule-week-schema-row-item-time">
+       {item.startTime} <i class="fa-solid fa-arrow-right-long"></i> {item.endTime}
+        </div>
+     </div>
+    {/each}
+</div> 
