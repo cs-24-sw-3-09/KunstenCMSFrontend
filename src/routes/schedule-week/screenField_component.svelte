d9d68f0a9708f1bc869f1f3f44b3da40798da74e
tobia <tlva23@student.aau.dk>
2024-11-13 13:29:59 +0100

    Props

--- src/routes/schedule-week/screenField_component.svelte
+++ src/routes/schedule-week/screenField_component.svelte
@@ -1,17 +1,15 @@
 <script>
-
     let props = $props();
 
-import TimeSlotCard from "./timeslot_templet.svelte"
-
+    import TimeSlotCard from "./timeslot_templet.svelte";
 </script>
-
 
 <div class="schedule-week-schema-row-day">
     <div class="schedule-week-schema-row-item-add">
-      <i class="fa-solid fa-plus"></i>
+        <i class="fa-solid fa-plus"></i>
     </div>
+
     {#each props.fields as timeslot}
-        <TimeSlotCard title={timeslot.title} color={timeslot.color}/>
+        <TimeSlotCard {timeslot} />
     {/each}
 </div>
--- src/routes/schedule-week/timeslot_templet.svelte
+++ src/routes/schedule-week/timeslot_templet.svelte
@@ -1,44 +1,17 @@
 <script>
-
-    //import from Database 
-    //import {timeslot}
-
-    let props = $props();
-    
-    let scheduleItems = [
-    {
-      title: "Default 2024",
-      startTime: "6:00",
-      endTime: "19:00",
-      color: "schedule-color-blue",
-      size: "schedule-size-3",
-    },
-    
-  ];
-
-/*
-    let timeSlots = &state([]);
-    timeslot.subscribe((value)) => {
-        timeSlots = value;
-    }
-
-    */
-
-
-
-
+    let { timeslot } = $props();
 </script>
 
-
 <div class="schedule-container">
-    {#each scheduleItems as item}
-     <div class={`schedule-week-schema-row-item ${props.color} ${item.size}`}>
+    <div
+        class={`schedule-week-schema-row-item ${timeslot.color} ${timeslot.size}`}
+    >
         <div class="schedule-week-schema-row-item-title">
-          {props.title}
-      </div>
-     <div class="schedule-week-schema-row-item-time">
-       {item.startTime} <i class="fa-solid fa-arrow-right-long"></i> {item.endTime}
+            {timeslot.title}
         </div>
-     </div>
-    {/each}
-</div> 
+        <div class="schedule-week-schema-row-item-time">
+            {timeslot.startTime} <i class="fa-solid fa-arrow-right-long"></i>
+            {timeslot.endTime}
+        </div>
+    </div>
+</div>
