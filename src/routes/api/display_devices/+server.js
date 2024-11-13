49eefedec2b2f89a4c2f227baee98806f53a3ee5
naitsa <naitsabot@gmail.com>
2024-11-13 10:58:52 +0100

    .

--- src/routes/api/display_devices/+server.js
+++
@@ -1,5 +0,0 @@
-// +server.js
-export async function GET() {
-    const data = await fetchDataFromDatabase(); // Indiker database fetch funktion
-    return new Response(JSON.stringify(data), { status: 200 });
-}
