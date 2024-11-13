60b059c211359fbae308ac251e0d8b2dec2d40e2
tobia <tlva23@student.aau.dk>
2024-11-13 14:54:56 +0100

    Started to component schedule day

---
+++ src/routes/schedule-day/+page.svelte
@@ -0,0 +1,57 @@
+
+<script>
+
+
+
+import Screen from "./screen_component_day.svelte"
+import TimeHeader from "./timeHeader_component.svelte"
+
+
+</script>
+
+
+
+
+
+    <div class="grid-container">
+      <div class="main-content">
+        <div class="schedule-day">
+          <!--Header-->
+          <div class="schedule-day-header">
+            <div class="schedule-day-header-top">
+              <div class="schedule-day-header-top-left">Oct. 23rd, 2024</div>
+              <div class="schedule-week-header-middle">
+                <a
+                  href="/schedule-week"
+                  class="schedule-day-header-week-button tooltippable tooltipText-Change-to-week-schedule"
+                  >Change to Week Schedule</a
+                >
+              </div>
+              <div class="schedule-day-header-top-right">
+                <button class="tooltippable tooltipText-Today">Today</button>
+                <button class="tooltippable tooltipText-previous-day"><i class="fa-solid fa-chevron-left"></i></button>
+                <button class="tooltippable tooltipText-Next-day"><i class="fa-solid fa-chevron-right"></i></button>
+              </div>
+            </div>
+          </div>
+
+          <TimeHeader/>
+          <Screen/>
+
+          <div class="schedule-day-body">
+
+            <div class="schedule-day-body-schemas">
+
+
+                <div class="schedule-day-body-row-add">hallalallala</div>
+              </div>
+
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+<style>
+    @import "page.css";
+</style>
---
+++ src/routes/schedule-day/timeHeader_component.svelte
@@ -0,0 +1,17 @@
+<script>
+
+    let time = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22 ,23];
+
+
+</script>
+
+<div class="schedule-day-body-header">
+    <div class="schedule-day-blank"></div>
+    <div class="schedule-day-times">
+        {#each time as times}
+        <div class="schedule-day-time"> {times + ":00"}</div>
+        {/each}
+    </div>
+  </div>
+
+
