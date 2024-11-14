9d438f6db2dd68b80e394a073ab8b0043cd1886d
tobia <tlva23@student.aau.dk>
2024-11-14 15:02:09 +0100

    Start making timeslot in day dynamic

--- src/routes/schedule-day/+page.svelte
+++ src/routes/schedule-day/+page.svelte
@@ -1,11 +1,37 @@
 
 <script>
 
+  let props = $props();
+
+  let data = [{
+    screens:  [{
+      id: [{
+        endTime: "09:00",
+        startTime: "07:00",
+        title: "test",
+        prioritize: 1
+
+        },
+        {
+        endTime: "17:00",
+        startTime: "10:00",
+        title: "test",
+        prioritize: 1
+        }
+
+      ]
+
+  }]
+  }]
+
 
 
 import Screen from "./screen_component_day.svelte"
 import TimeHeader from "./timeHeader_component.svelte"
 
+
+let color = "red";
+let size = "10px"; 
 
 </script>
 
@@ -32,11 +58,18 @@ import TimeHeader from "./timeHeader_component.svelte"
                 <button class="tooltippable tooltipText-previous-day"><i class="fa-solid fa-chevron-left"></i></button>
                 <button class="tooltippable tooltipText-Next-day"><i class="fa-solid fa-chevron-right"></i></button>
               </div>
+              <div style="--theme-color: {color}; --theme-size: {size};">
+                <p class="timeslot">
+                  the color is set using a CSS variable
+                </p>
+              </div>
             </div>
           </div>
 
           <TimeHeader/>
-          <Screen/>
+          {#each data as device}
+          <Screen {device}/> 
+          {/each} 
 
           <div class="schedule-day-body">
 
@@ -54,4 +87,10 @@ import TimeHeader from "./timeHeader_component.svelte"
 
 <style>
     @import "page.css";
+.timeslot{
+  color: var(--theme-color);
+  font-size: var(--theme-size);
+
+}
+
 </style>
--- src/routes/schedule-day/timeslot_templet_day.svelte
+++ src/routes/schedule-day/timeslot_templet_day.svelte
@@ -1,17 +1,76 @@
 
 <script>
 
+  let props = $props();
+
+  function constructTimeslot(startTime, endTime, bgcolor, color, prioritize){
+    
+
+
+  }
+
+  let startTime = "06:00";
+  let endTime = "12:00";
+  let title = "Test"
+
+
+  function timeToPercentage(time) {
+    const parts = time ? time.split(":") : [0, 0];
+    const hours = parseInt(parts[0], 10) || 0;
+    const minutes = parseInt(parts[1], 10) || 0;
+    return (hours + minutes / 60) * 4.166;
+  }
+
+
+  console.log(timeToPercentage(startTime));
+  console.log(timeToPercentage(endTime));
+
+
+  let start = timeToPercentage(startTime) + "%";
+  let end = timeToPercentage(endTime) - (timeToPercentage(startTime)) + "%";
+  let color = "black";
+  let bgcolor = "lightblue";
+
 
 
 </script>
-
-
-        <div class="schedule-day-timeslot timeslot3">
+        <div class="schedule-day timeslot" style="--theme-color: {color}; --theme-bgcolor: {bgcolor}; --theme-left: {start};--theme-right: {end}">
         <div class="schedule-day-timeslot-title">
-          Default 2024
+          {title}
         </div>
         <div class="schedule-day-timeslot-time">
-        6:00 <i class="fa-solid fa-arrow-right-long"></i> 19:00
+        {startTime} <i class="fa-solid fa-arrow-right-long"></i> {endTime}
         </div>
     </div>
+
+
+
+  <style>
+
+.schedule-day {
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    cursor: pointer;
+    z-index: 1;
+  }
+
+  .timeslot{
+  color: var(--theme-color);
+  height: 100%;
+  background-color: var(--theme-bgcolor);
+  left: var(--theme-left);
+  width: var(--theme-right);
+
+
+
+
+}
+
+
+
+  </style>
+
+
     
