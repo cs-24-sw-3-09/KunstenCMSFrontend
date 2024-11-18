625c158c0ba9aee7943f6e1b2b8cc5b7c60788c1
tobia <tlva23@student.aau.dk>
2024-11-18 10:39:18 +0100

    Dates updates dynamically

--- src/routes/schedule-day/timeHeader_component.svelte
+++ src/routes/schedule-day/timeHeader_component.svelte
@@ -1,12 +1,13 @@
 <script>
 
+let currentDay1 = new Date();
 import Buttom from "./buttom.component.svelte"
 
     let time = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22 ,23];
 
     
-function getWeekRange() {
-    const today = new Date();
+function getWeekRange(input) {
+    const today = new Date(input);
   
     
     const getDayOrdinal = (day) => {
@@ -24,8 +25,21 @@ function getWeekRange() {
     return (`${formatDate(today)} ${today.getFullYear()}`);
 }
 
+  // Function to shift the current date by a number of weeks
+  function shiftWeek(shift) {
+    // Add or subtract 7 days (one week) and reassign currentDate
+    const newDate = new Date(currentDay1);
+    newDate.setDate(currentDay1.getDate() + shift);
+    currentDay1 = newDate; // Reassign to trigger reactivity
+  }
 
-let day = getWeekRange();
+    // Function to reset to the current date's week
+    function resetToCurrentWeek() {
+    currentDay1 = new Date(); // Reset to today
+  }
+
+
+$: day = getWeekRange(new Date(currentDay1));
 
 
 </script>
@@ -34,11 +48,8 @@ let day = getWeekRange();
 <div class="schedule-day-header">
     <div class="schedule-day-header-top">
       <div class="schedule-day-header-top-left">{day}
-        <Buttom/>
+        <button class="gallery-header-upload-button">Add Timeslot</button>
       </div>
-      
-
-
       
 
       <div class="schedule-week-header-middle">
@@ -48,10 +59,12 @@ let day = getWeekRange();
           >Change to Week Schedule</a
         >
       </div>
-      <div class="schedule-day-header-top-right">
-        <button class="tooltippable tooltipText-Today">Today</button>
-        <button class="tooltippable tooltipText-previous-day"><i class="arrow left"></i></button>
-        <button class="tooltippable tooltipText-Next-day"><i class="arrow right"></i></button>
+      <div class="schedule-week-header-top-right">
+        <button on:click={resetToCurrentWeek}>Today</button>
+        <!-- svelte-ignore a11y_consider_explicit_label -->
+        <button on:click={() => shiftWeek(-1)}><i class="arrow left"></i></button>
+        <!-- svelte-ignore a11y_consider_explicit_label -->
+        <button on:click={() => shiftWeek(1)}><i class="arrow right"></i></button>
       </div>
   </div>
 </div>
