48b039c8078cffcb852ebaa52a625970fdf1dbfc
naitsa <naitsabot@gmail.com>
2024-11-18 12:49:36 +0100

    App tab titles and desctiption

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,13 @@
 <script>
     let { children } = $props();
+    import { page } from "$app/stores";
+    import logo from "$lib/images/logo.ico";
+
+    const title = "Kunsten CMS";
+    let subtitle = ($page.url.pathname.split("/").slice(1).toString()[0].toUpperCase() + $page.url.pathname.split("/").slice(1).toString().slice(1)).replace(/_/g, " ").replace(/-/g, " ");
+    let pagetitle =  [subtitle, title].filter(Boolean).join(" - ");
+
+    const desctiption = "Content Management System for Kunsten Museum of Modern Art Aalborg";
 </script>
 
 <svelte:head>
@@ -7,6 +15,9 @@
         src="https://kit.fontawesome.com/86cff0f4ad.js"
         crossorigin="anonymous"
     ></script>
+    <link rel="icon" type="image/x-icon" href={logo}>
+    <title>{pagetitle}</title>
+    <meta name="description" content={desctiption} />
 </svelte:head>
 
 {@render children()}
