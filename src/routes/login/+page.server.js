bf0c94a9f7faa5fde8e53addbf4c9d693e8c98eb
naitsa <naitsabot@gmail.com>
2024-11-20 14:44:43 +0100

    remapping and rename token

--- src/routes/login/+page.server.js
+++ src/routes/login/+page.server.js
@@ -1,9 +1,53 @@
 import { redirect } from '@sveltejs/kit';
-import jwt from "jsonwebtoken";
+/* import jwt from "jsonwebtoken"; */
+
+
+// https://authjs.dev/reference/sveltekit
+
+
+
+/* export const load = async ({ cookies }) => {
+	const authToken = cookies.get('authToken');
+	if (authToken) {
+		const decoded = jwt.verify(authToken, process.env.JWT_SECRET);
+		return {
+			user: decoded
+		};
+	}
+	return {
+		user: undefined
+	};
+}; */
 
 export const actions = {
+	/* login: async ({ cookies, request }) => {
+		const loginFormData = await request.formData();
+		const { email } = loginFormData.get("email").toString();
+		const { password } = loginFormData.get("password").toString();
+
+		const loginResponse = {
+			email,
+			password,
+			error: false,
+			message: ""
+		}
+
+		try {
+			const userAttemptingLogin = await fetch(APIURL + "/api/login", {
+				method: "POST",
+				headers: {
+					"Content-type": "application/json",
+				},
+				body: JSON.stringify({ email, password }),
+			});
+			
+		}
+		
+	}, */
+
+	// Set authToken and redirect to dashboard
 	default: ({ cookies, url }) => {
-		cookies.set('logged_in', 'true', { path: '/' });
+		cookies.set('authToken', 'true', { path: '/' });
 		redirect(303, url.searchParams.get('redirectTo') ?? '/dashboard');
 	}
 };
