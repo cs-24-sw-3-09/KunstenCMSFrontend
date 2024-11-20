08fd178be47f7d72dfd371faa6078d9fd8dc942e
naitsa <naitsabot@gmail.com>
2024-11-20 11:43:28 +0100

    preliminary auth

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,29 +1,5 @@
-<script>
-    import Testmodal from '$lib/components/modal/testmodal.svelte';
-    import { onMount } from 'svelte';
-    
-    let showTestModal = false;
+<h1>log in</h1>
 
-    onMount(() => {
-        showTestModal = true;
-    });
-
-    function toggleTestModal() {
-        showTestModal = !showTestModal;
-    }
-
-    function submitTestModal(event) {
-        event.preventDefault();
-        let form = new FormData(event.target);
-        console.log(form);
-        toggleTestModal();
-    }
-
-</script>
-
-<h1>Modal Page</h1>
-<button onclick={toggleTestModal}>Show Test Modal</button>
-
-{#if showTestModal}
-    <Testmodal doClose={toggleTestModal} doSubmit={submitTestModal}/>
-{/if}
+<form method="POST">
+	<button>log in</button>
+</form>
