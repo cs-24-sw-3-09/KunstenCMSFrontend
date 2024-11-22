de9b90b747d653e8bbcf6f267f63bcca36c6a9b8
naitsa <naitsabot@gmail.com>
2024-11-22 13:00:36 +0100

    something

--- src/routes/(authed)/gallery/+page.svelte
+++ src/routes/(authed)/gallery/+page.svelte
@@ -1,1 +1,18 @@
-<div>Hello from gallery/+page.svelte</div>
+<script>
+
+</script>
+
+<div class="main-content">
+    <div class="page">
+        <div class="page-header">
+            <h1>Gallery</h1>
+        </div>
+        <!-- <Gallery {slideshows} doClose={} doSubmit={} /> -->
+    </div>
+</div>
+
+
+
+<style>
+    @import "$lib/styles/gallery.css";
+</style>
