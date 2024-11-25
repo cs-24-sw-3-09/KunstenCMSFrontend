1c7e58b9fe4fb841f6d9da2680a278a1e4551fd5
naitsa <naitsabot@gmail.com>
2024-11-25 16:00:14 +0100

    new grouping, rememberd redirect fix

--- src/routes/(authed)/+layout.server.js
+++ src/routes/(authed)/+layout.server.js
@@ -2,6 +2,6 @@ import {redirect} from "@sveltejs/kit"
 
 export function load({ cookies, url }) {
 	if (!cookies.get("authToken")) {
-		redirect(303, `/login?redirectTo=${url.pathname}"`);
+		redirect(303, `/login?redirectTo=${url.pathname}`);
 	}
 }
