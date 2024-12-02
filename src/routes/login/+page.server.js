fa441c3948da125f99d55671577a19fd10540f01
Simon Stjernholm <simon@stjernholm.dev>
2024-12-03 00:52:22 +0100

    should be returned?

--- src/routes/login/+page.server.js
+++ src/routes/login/+page.server.js
@@ -67,7 +67,7 @@ export const actions = {
 				maxAge: 60 * 60 * 24 * 7 // 7 days
 			}); 
 		
-			redirect(303, url.searchParams.get('redirectTo') ?? '/dashboard');
+			return redirect(303, url.searchParams.get('redirectTo') ?? '/dashboard');
 		}		
 	},
 };
