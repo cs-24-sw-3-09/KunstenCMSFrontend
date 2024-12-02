bb459dce42bd658df54c0a3988da87141f894044
Simon Stjernholm <simon@stjernholm.dev>
2024-12-03 00:23:04 +0100

    try throw

--- src/routes/login/+page.server.js
+++ src/routes/login/+page.server.js
@@ -67,7 +67,7 @@ export const actions = {
 				maxAge: 60 * 60 * 24 * 7 // 7 days
 			}); 
 		
-			redirect(303, url.searchParams.get('redirectTo') ?? '/dashboard');
+			throw redirect(303, url.searchParams.get('redirectTo') ?? '/dashboard');
 		}		
 	},
 };
