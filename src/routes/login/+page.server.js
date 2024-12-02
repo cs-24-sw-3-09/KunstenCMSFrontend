4b2614c610d36f7b1df03e5c4b63d5c72d97593d
Simon Stjernholm <simon@stjernholm.dev>
2024-12-02 23:20:31 +0100

    Revert "try fix with axios"

--- src/routes/login/+page.server.js
+++ src/routes/login/+page.server.js
@@ -1,5 +1,4 @@
-import { error, fail, redirect } from '@sveltejs/kit';
-import axios from 'axios';
+import { fail, redirect } from '@sveltejs/kit';
 
 import { env } from "$env/dynamic/private";
 
@@ -16,7 +15,7 @@ export const load = async ({ cookies }) => {
 
 /** @type {import("./$types").Actions} */
 export const actions = {
-	login: async ({ cookies, url, request }) => {
+	login : async ({ cookies, url, request }) => {
 		const formData = await request.formData();
 
 		const email = formData.get("email").toString();
@@ -26,42 +25,49 @@ export const actions = {
 			email,
 			password,
 		};
-
+		
 		// Validate the email and password fields
-		if (!loginRequestBody.email || !loginRequestBody.password) {
-			return fail(400, { error: "Email and password are required." });
-		}
+        if (!loginRequestBody.email || !loginRequestBody.password) {
+            return fail(400, { error: "Email and password are required." });
+        }
 
 		// Login request
-		try {
-			let token = await axios.post(API_URL + "/api/account/login", loginRequestBody, {
-				headers: {
-					"Content-type": "application/json",
-				},
-			}).then(response => {
-				return response.data; // Extract the jwt token as plain text
-			});
+		const loginResponse = await fetch(API_URL + "/api/account/login", {
+			method: "POST",
+			headers: {
+				"Content-type": "application/json",
+			},
+			body: JSON.stringify(loginRequestBody),
+		});
 
-			// Set the jwt token as a cookie
-			cookies.set("authToken", token, {
-				path: '/',
-				httpOnly: true, // Prevent JavaScript access. Good practice to prevent XSS attacks
-				secure: true, // Ensures the cookie is sent only over HTTPS
-				sameSite: 'strict', // To prevent the cookie from being sent in cross-site requests
-				maxAge: 60 * 60 * 24 * 7 // 7 days
-			});
-		} catch (error) {
+		//console.log("loginResponsestart", loginResponse,"loginResponseend");		
+
+		if (loginResponse.status !== 200) {
 			try {
 				cookies.delete('authToken', { path: '/' });
 			} catch (e) {
 				console.error(e);
 			}
-			return fail(error.status || 500, {
+
+			return fail(loginResponse.status, {
 				error: true,
 				message: "Incorrect email or password."
 			});
 		}
 
-		redirect(303, url.searchParams.get('redirectTo') ?? '/dashboard');
+		if (loginResponse.status === 200) {
+			const token = await loginResponse.text(); // Extract the jwt token as plain text
+		
+			// Set the jwt token as a cookie
+			cookies.set("authToken", token, { 
+				path: '/',
+				httpOnly: true, // Prevent JavaScript access. Good practice to prevent XSS attacks
+				secure: true, // Ensures the cookie is sent only over HTTPS
+				sameSite: 'strict', // To prevent the cookie from being sent in cross-site requests
+				maxAge: 60 * 60 * 24 * 7 // 7 days
+			}); 
+		
+			redirect(303, url.searchParams.get('redirectTo') ?? '/dashboard');
+		}		
 	},
 };
