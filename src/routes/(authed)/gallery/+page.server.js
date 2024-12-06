1a34ed34603bebf4dd6d39b56fdfef2326bee52e
naitsa <naitsabot@gmail.com>
2024-12-06 13:58:22 +0100

    Det burder virke men der er noget backend

--- src/routes/(authed)/gallery/+page.server.js
+++ src/routes/(authed)/gallery/+page.server.js
@@ -108,6 +108,8 @@ export const actions = {
             body: formData,
         });
 
+        /* console.log(formData); */
+
         const responseData = await response.json();
 
         responseData.src = env.CLIENT_API_URL + responseData.location;
@@ -201,15 +203,22 @@ export const actions = {
     replaceVisualMedia: async ({ cookies, url, request }) => {
         const formData = await request.formData();
 
+        let requestBody = new FormData();
+        requestBody.append("file", formData.get("file"));
+        console.log(requestBody);
         
-        const response = await fetch(env.SERVER_API_URL + "/api/" + formData.get("id") + "file", {
+        console.log(env.SERVER_API_URL + "/api/visual_medias/" + formData.get("id") + "/file");
+        
+        const response = await fetch(env.SERVER_API_URL + "/api/" + formData.get("id") + "/file", {
             method: "POST",
             headers: {
                 /* "Content-type": "multipart/form-data", */
                 "Authorization": "Bearer " + cookies.get("authToken"),
             },
-            body: formData,
+            body: requestBody,
         });
+
+        console.log(response);
 
         if (!(response.status >= 200 && response.status < 300)) {
             return fail(response.status, { error: "Failed to replace visual media." });
