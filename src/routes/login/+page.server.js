d3de837f7412a0725c35507afdfadaded42a17b1
Simon Stjernholm <simon@stjernholm.dev>
2025-01-29 21:57:39 +0100

    should work

--- src/routes/login/+page.server.js
+++ src/routes/login/+page.server.js
@@ -58,7 +58,7 @@ export const actions = {
 			cookies.set("authToken", token, { 
 				path: '/',
 				httpOnly: false, // Would prevent JavaScript access. Good practice to prevent XSS attacks
-				secure: true, // Ensures the cookie is sent only over HTTPS
+				secure: false, // Ensures the cookie is sent only over HTTPS
 				sameSite: 'strict', // To prevent the cookie from being sent in cross-site requests
 				maxAge: 60 * 60 * 24 * 7 // 7 days
 			}); 
